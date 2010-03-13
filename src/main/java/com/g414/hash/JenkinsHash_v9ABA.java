6690089a4f9430b04dc76fda227d57efe75805c3
Sunny Gleason <sunny.gleason@gmail.com>
2010-03-13 12:16:24 -0500

    reformatting tabs -> spaces

--- src/main/java/com/g414/hash/JenkinsHash_v9ABA.java
+++ src/main/java/com/g414/hash/JenkinsHash_v9ABA.java
@@ -18,18 +18,19 @@
 package com.g414.hash;
 
 /**
- * <pre>lookup8.c, by Bob Jenkins, May 2006, Public Domain.
- *
+ * <pre>
+ * lookup8.c, by Bob Jenkins, May 2006, Public Domain.
+ * 
  * You can use this free for any purpose. It's in the public domain.
  * It has no warranty.
  * </pre>
- *
+ * 
  * @see <a href="http://burtleburtle.net/bob/c/lookup3.c">lookup3.c</a>
  * @see <a href="http://www.ddj.com/184410284">Hash Functions (and how this
- * function compares to others such as CRC, MD?, etc</a>
+ *      function compares to others such as CRC, MD?, etc</a>
  * @see <a href="http://burtleburtle.net/bob/hash/doobs.html">Has update on the
- * Dr. Dobbs Article</a> 
-*/
+ *      Dr. Dobbs Article</a>
+ */
 public class JenkinsHash_v9ABA implements LongHash {
 	/** @see LongHash#getName() */
 	@Override
@@ -49,7 +50,7 @@ public class JenkinsHash_v9ABA implements LongHash {
 		if (k < 1) {
 			throw new IllegalArgumentException("k must be >= 1");
 		}
-		
+
 		long[] hashCodes = new long[k];
 		byte[] representation = object.getBytes();
 
@@ -61,32 +62,30 @@ public class JenkinsHash_v9ABA implements LongHash {
 	}
 
 	/*
-	--------------------------------------------------------------------
-	hash() -- hash a variable-length key into a 64-bit value
-	  k     : the key (the unaligned variable-length array of bytes)
-	  level : can be any 8-byte value
-	Returns a 64-bit value.  Every bit of the key affects every bit of
-	the return value.  No funnels.  Every 1-bit and 2-bit delta achieves
-	avalanche.  About 41+5len instructions.
-
-	The best hash table sizes are powers of 2.  There is no need to do
-	mod a prime (mod is sooo slow!).  If you need less than 64 bits,
-	use a bitmask.  For example, if you need only 10 bits, do
-	  h = (h & hashmask(10));
-	In which case, the hash table should have hashsize(10) elements.
-
-	If you are hashing n strings (ub1 **)k, do it like this:
-	  for (i=0, h=0; i<n; ++i) h = hash( k[i], len[i], h);
-
-	By Bob Jenkins, Jan 4 1997.  bob_jenkins@burtleburtle.net.  You may
-	use this code any way you wish, private, educational, or commercial,
-	but I would appreciate if you give me credit.
-
-	See http://burtleburtle.net/bob/hash/evahash.html
-	Use for hash table lookup, or anything where one collision in 2^^64
-	is acceptable.  Do NOT use for cryptographic purposes.
-	--------------------------------------------------------------------
-	*/
+	 * --------------------------------------------------------------------
+	 * hash() -- hash a variable-length key into a 64-bit value k : the key (the
+	 * unaligned variable-length array of bytes) level : can be any 8-byte value
+	 * Returns a 64-bit value. Every bit of the key affects every bit of the
+	 * return value. No funnels. Every 1-bit and 2-bit delta achieves avalanche.
+	 * About 41+5len instructions.
+	 * 
+	 * The best hash table sizes are powers of 2. There is no need to do mod a
+	 * prime (mod is sooo slow!). If you need less than 64 bits, use a bitmask.
+	 * For example, if you need only 10 bits, do h = (h & hashmask(10)); In
+	 * which case, the hash table should have hashsize(10) elements.
+	 * 
+	 * If you are hashing n strings (ub1 **)k, do it like this: for (i=0, h=0;
+	 * i<n; ++i) h = hash( k[i], len[i], h);
+	 * 
+	 * By Bob Jenkins, Jan 4 1997. bob_jenkins@burtleburtle.net. You may use
+	 * this code any way you wish, private, educational, or commercial, but I
+	 * would appreciate if you give me credit.
+	 * 
+	 * See http://burtleburtle.net/bob/hash/evahash.html Use for hash table
+	 * lookup, or anything where one collision in 2^^64 is acceptable. Do NOT
+	 * use for cryptographic purposes.
+	 * --------------------------------------------------------------------
+	 */
 	public long computeJenkinsHash(byte[] k, long level) {
 		/* Set up the internal state */
 		long a = level;
@@ -108,22 +107,46 @@ public class JenkinsHash_v9ABA implements LongHash {
 					+ ((long) k[i + 18] << 16) + ((long) k[i + 19] << 24)
 					+ ((long) k[i + 20] << 32) + ((long) k[i + 21] << 40)
 					+ ((long) k[i + 22] << 48) + ((long) k[i + 23] << 56));
-			
+
 			/* mix64(a, b, c); */
-			a -= b; a -= c; a ^= (c>>43);
-			b -= c; b -= a; b ^= (a<<9);
-			c -= a; c -= b; c ^= (b>>8);
-			a -= b; a -= c; a ^= (c>>38);
-			b -= c; b -= a; b ^= (a<<23);
-			c -= a; c -= b; c ^= (b>>5); 
-			a -= b; a -= c; a ^= (c>>35);
-			b -= c; b -= a; b ^= (a<<49);
-			c -= a; c -= b; c ^= (b>>11);
-			a -= b; a -= c; a ^= (c>>12);
-			b -= c; b -= a; b ^= (a<<18);
-			c -= a; c -= b; c ^= (b>>22);
+			a -= b;
+			a -= c;
+			a ^= (c >> 43);
+			b -= c;
+			b -= a;
+			b ^= (a << 9);
+			c -= a;
+			c -= b;
+			c ^= (b >> 8);
+			a -= b;
+			a -= c;
+			a ^= (c >> 38);
+			b -= c;
+			b -= a;
+			b ^= (a << 23);
+			c -= a;
+			c -= b;
+			c ^= (b >> 5);
+			a -= b;
+			a -= c;
+			a ^= (c >> 35);
+			b -= c;
+			b -= a;
+			b ^= (a << 49);
+			c -= a;
+			c -= b;
+			c ^= (b >> 11);
+			a -= b;
+			a -= c;
+			a ^= (c >> 12);
+			b -= c;
+			b -= a;
+			b ^= (a << 18);
+			c -= a;
+			c -= b;
+			c ^= (b >> 22);
 			/* mix64(a, b, c); */
-			
+
 			i += 24;
 			len -= 24;
 		}
@@ -181,22 +204,46 @@ public class JenkinsHash_v9ABA implements LongHash {
 			a += ((long) k[i]);
 			/* case 0: nothing left to add */
 		}
-		
+
 		/* mix64(a, b, c); */
-		a -= b; a -= c; a ^= (c>>43);
-		b -= c; b -= a; b ^= (a<<9);
-		c -= a; c -= b; c ^= (b>>8);
-		a -= b; a -= c; a ^= (c>>38);
-		b -= c; b -= a; b ^= (a<<23);
-		c -= a; c -= b; c ^= (b>>5); 
-		a -= b; a -= c; a ^= (c>>35);
-		b -= c; b -= a; b ^= (a<<49);
-		c -= a; c -= b; c ^= (b>>11);
-		a -= b; a -= c; a ^= (c>>12);
-		b -= c; b -= a; b ^= (a<<18);
-		c -= a; c -= b; c ^= (b>>22);
+		a -= b;
+		a -= c;
+		a ^= (c >> 43);
+		b -= c;
+		b -= a;
+		b ^= (a << 9);
+		c -= a;
+		c -= b;
+		c ^= (b >> 8);
+		a -= b;
+		a -= c;
+		a ^= (c >> 38);
+		b -= c;
+		b -= a;
+		b ^= (a << 23);
+		c -= a;
+		c -= b;
+		c ^= (b >> 5);
+		a -= b;
+		a -= c;
+		a ^= (c >> 35);
+		b -= c;
+		b -= a;
+		b ^= (a << 49);
+		c -= a;
+		c -= b;
+		c ^= (b >> 11);
+		a -= b;
+		a -= c;
+		a ^= (c >> 12);
+		b -= c;
+		b -= a;
+		b ^= (a << 18);
+		c -= a;
+		c -= b;
+		c ^= (b >> 22);
 		/* mix64(a, b, c); */
-		
+
 		return c;
 	}
 }
